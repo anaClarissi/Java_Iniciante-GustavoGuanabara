01c217a773e339d55765ad8720f88ade8663fb6f
Ana Clarissi <anaclarissicruz@gmail.com>
2025-08-30 20:05:49 -0300

    RepeticaoFor.java aula13

---
+++ Aula13/RepeticaoFor/src/main/java/RepeticaoFor.java
@@ -0,0 +1,13 @@
+public class RepeticaoFor {
+
+    public static void main(String[] args) {
+
+        for (int cc = 15; cc >= 5; cc -= 2){
+
+            System.out.println(cc);
+
+        }
+
+    }
+
+}
